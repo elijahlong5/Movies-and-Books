727f1db68687797f8d943eeba8f51f786471bc64
Elijah <elijah498@gmail.com>
2020-06-01 16:25:21 -0400

    delete feature  Please enter the commit message for your changes. Lines starting

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Book } from "../models/book";
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Movie} from "../models/movie";
-
 
 @Injectable({
   providedIn: 'root'
